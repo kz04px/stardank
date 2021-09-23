3fd9ddfbc7d6e3ed773a10c725e47856725907c2
kz04px <6536046+kz04px@users.noreply.github.com>
2021-09-23 21:53:03 +0100

    Add command line parameters
    -v and -h

--- src/stardank/main.cpp
+++ src/stardank/main.cpp
@@ -11,6 +11,11 @@ int main(int argc, char** argv) {
     std::cout << "Debug build!\n\n";
 #endif
 
+    CLI::App cli{"Stardank"};
+    cli.set_version_flag("-v,--version", "Stardank v0.0.1", "Display Stardank version information");
+
+    CLI11_PARSE(cli, argc, argv);
+
     try {
         auto app = Application(argc, argv);
         app.run();
